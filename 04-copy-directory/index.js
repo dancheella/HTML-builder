56ab3793f1c302737249a9eda439009981642743
Denis Dankin <ulman33312@gmail.com>
2024-01-22 00:00:45 +0300

    feat: finish 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  try {
+    await fs.access(folderPath);
+  } catch (error) {
+    await fs.mkdir(folderPath, { recursive: true });
+  }
+  const files = await fs.readdir(path.join(__dirname, 'files'), {
+    withFileTypes: true,
+  });
+  for (const file of files) {
+    if (file.isFile()) {
+      const filePath = path.join(__dirname, 'files', file.name);
+      const newFilePath = path.join(folderPath, file.name);
+      await fs.copyFile(filePath, newFilePath);
+    }
+  }
+}
+
+copyDir().then(() => {
+  console.log('Done!');
+});
